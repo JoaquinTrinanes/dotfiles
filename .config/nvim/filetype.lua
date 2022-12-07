5d8b9c09dfe5d248afaafeac059de2472a533f95
Joaquín Triñanes <quin.trinanes@gmail.com>
2022-12-07 15:19:48 +0100

    Autodetect JSON files

--- .config/nvim/filetype.lua
+++ .config/nvim/filetype.lua
@@ -2,6 +2,15 @@ vim.filetype.add({
     pattern = {
         ['%.env'] = 'sh',
         ['%.env%..+'] = 'sh',
-        ['%..*rc'] = 'sh'
+        ['%..*rc'] = 'sh',
+        ['.*'] = {
+            priority = -math.huge,
+            function(_, bufnr, _)
+                local firstLine = vim.filetype.getlines(bufnr, 1)
+                if vim.filetype.matchregex(firstLine, "^\\s*[\\[\\{]") then
+                    return 'json'
+                end
+            end
+        }
     }
 })
