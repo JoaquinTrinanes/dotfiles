e34a4643c57730a2d2b4e9397d602b19b4889533
Joaquín Triñanes <quin.trinanes@gmail.com>
2022-12-11 02:22:34 +0100

    More cleanups

--- .config/shell/path.sh
+++ .config/shell/path.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
 
-export CONFIG_DIR=~/.config
-
-export XDG_CONFIG_HOME="$HOME/.config"
-export CONFIG_DIR=$XDG_CONFIG_HOME
-
 # Android
 export ANDROID_HOME="$HOME/Android/Sdk"
 export PATH="$ANDROID_HOME/emulator:$PATH"
@@ -14,8 +9,6 @@ export PATH="$ANDROID_HOME/platform-tools:$PATH"
 export ANDROID_SDK_ROOT="$HOME/Android/Sdk"
 # End Android
 
-export VIMCONFIG="$CONFIG_DIR/nvim/init.vim"
-
 # Rust
 [ -f $HOME/.cargo/env ] && . "$HOME/.cargo/env"
 
@@ -24,7 +17,7 @@ export GOPATH="$HOME/go"
 export PATH="$GOPATH/bin:$HOME/.local/bin:$PATH"
 
 [ -f /etc/profile.d/autojump.sh ] && . /etc/profile.d/autojump.sh
-[ -f /usr/local/etc/profile.d/autojump.sh ] && source /usr/local/etc/profile.d/autojump.sh
+[ -f /usr/local/etc/profile.d/autojump.sh ] && . /usr/local/etc/profile.d/autojump.sh
 
 if [ $IS_MAC ]; then
     export PNPM_HOME="/Users/joaquin/Library/pnpm"
