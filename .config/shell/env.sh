ae65339a1f45708ceed997336eddcd87b49f7198
Joaquín Triñanes <quin.trinanes@gmail.com>
2022-12-11 20:38:14 +0100

    Create helper to source parent direnv files

--- .config/shell/env.sh
+++ .config/shell/env.sh
@@ -8,6 +8,7 @@ export VISUAL="code"
 
 export XDG_CONFIG_HOME="$HOME/.config"
 export XDG_DATA_HOME="$HOME/.local/share"
+export XDG_CACHE_HOME="$HOME/.cache"
 export CONFIG_DIR=$XDG_CONFIG_HOME
 
 export VIMCONFIG="$CONFIG_DIR/nvim/init.vim"
