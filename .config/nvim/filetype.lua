f55310310062696918558b3a86045fb2615e8a82
Joaquín Triñanes <quin.trinanes@gmail.com>
2022-12-20 11:59:56 +0100

    Disable fallback filetype detection
    Patterns are evaluated before extensions and filenames, so this config
    would match toml files as json (for example)

--- .config/nvim/filetype.lua
+++ .config/nvim/filetype.lua
@@ -3,14 +3,5 @@ vim.filetype.add({
 		["%.env"] = "sh",
 		["%.env%..+"] = "sh",
 		["%..*rc"] = "sh",
-		[".*"] = {
-			priority = -math.huge,
-			function(_, bufnr, _)
-				local firstLine = vim.filetype.getlines(bufnr, 1)
-				if vim.filetype.matchregex(firstLine, "^\\s*[\\[\\{]") then
-					return "json"
-				end
-			end,
-		},
 	},
 })
