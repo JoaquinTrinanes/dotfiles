da97a255d90cc388f0622454c6ef24fea1536237
Joaquín Triñanes <quin.trinanes@gmail.com>
2022-12-28 11:58:22 +0100

    Better wezterm font fallback

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -2,7 +2,12 @@ local wez = require("wezterm")
 
 return {
 	color_scheme = "nord",
-	font = wez.font_with_fallback({ "Fira Code", "Symbols Nerd Font", "DejaVu Sans Mono" }),
+	font = wez.font_with_fallback({
+		"Fira Code",
+		{ family = "JoyPixels", assume_emoji_presentation = true },
+		{ family = "Symbols Nerd Font", assume_emoji_presentation = true },
+		"DejaVu Sans Mono",
+	}),
 	font_size = 16,
 	use_fancy_tab_bar = false,
 	default_cursor_style = "SteadyBar",
