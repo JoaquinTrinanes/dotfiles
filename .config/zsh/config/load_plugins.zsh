a373f0eabbe2837d22081569d543ad6a43a4be06
Joaquín Triñanes <quin.trinanes@gmail.com>
2023-01-03 10:34:31 +0100

    Automaticallyinstall antidote in ZSH

--- .config/zsh/config/load_plugins.zsh
+++ .config/zsh/config/load_plugins.zsh
@@ -1,14 +1,27 @@
 # Some plugin config must be set before they are loaded
 for f in $(dirname "$0")/plugin_config/*.zsh; do
-    . $f
+  . $f
 done
 
 typeset -gU cdpath fpath mailpath path
 
-# You can change the names/locations of these if you prefer.
 antidote_dir=${ZDOTDIR:-~}/.antidote
+plugins_txt=${ZDOTDIR:-~}/zsh_plugins.txt
+static_file=${ZDOTDIR:-~}/.zsh_plugins.zsh
 
-source $antidote_dir/antidote.zsh
-antidote load
+# Clone antidote if necessary and generate a static plugin file.
+if [[ ! $static_file -nt $plugins_txt ]]; then
+  [[ -e $antidote_dir ]] ||
+    git clone --depth=1 https://github.com/mattmc3/antidote.git $antidote_dir
+  (
+    source $antidote_dir/antidote.zsh
+    [[ -e $plugins_txt ]] || touch $plugins_txt
+    antidote bundle <$plugins_txt >$static_file
+  )
+fi
 
-unset antidote_dir
+autoload -Uz $antidote_dir/functions/antidote
+
+source $static_file
+
+unset antidote_dir plugins_txt static_file
