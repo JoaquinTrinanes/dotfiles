3e42fa988561ee446d6742c7eff65691c092768c
Joaquín Triñanes <quin.trinanes@gmail.com>
2023-02-09 11:24:14 +0100

    Fix noice overriding hover

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -349,6 +349,7 @@ lvim.plugins = {
 				signature = {
 					enabled = false,
 				},
+				hover = { enabled = false },
 				override = {
 					["vim.lsp.util.convert_input_to_markdown_lines"] = true,
 					["vim.lsp.util.stylize_markdown"] = true,
