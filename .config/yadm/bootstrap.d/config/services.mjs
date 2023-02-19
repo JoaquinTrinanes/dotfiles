7b0af75e70fba4db5d2fe514ac1d4842a319a8b6
Joaquín Triñanes <quin.trinanes@gmail.com>
2023-02-19 23:31:42 +0100

    Define systemd services in packages.yaml

--- .config/yadm/bootstrap.d/config/services.mjs
+++ .config/yadm/bootstrap.d/config/services.mjs
@@ -1,9 +1,15 @@
 #!/usr/bin/env -S zx --experimental
 
+import { packages } from "../utils/packages.mjs";
 import { enableService } from "../utils/services.mjs";
 
-const userServices = ["pueued"];
+const userServices = packages.services.user;
+const systemServices = packages.services.system;
 
 for await (const service of userServices) {
   await enableService(service, { user: true, now: true });
+}
+
+for await (const service of systemServices) {
+  await enableService(service, { user: false, now: true });
 }
