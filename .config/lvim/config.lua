e725a9465ea3af0fe0902f8801ab8e4b2d683eb9
Joaquín Triñanes <hi@joaquint.io>
2023-06-26 08:34:13 +0200

    Wezterm + nvim split navigation

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -534,6 +534,26 @@ lvim.plugins = {
 			require("null-ls").register(require("nu-ls"))
 		end,
 	},
+	{
+		"mrjones2014/smart-splits.nvim",
+		version = "*",
+		config = function()
+			vim.keymap.set("n", "<A-h>", require("smart-splits").resize_left)
+			vim.keymap.set("n", "<A-j>", require("smart-splits").resize_down)
+			vim.keymap.set("n", "<A-k>", require("smart-splits").resize_up)
+			vim.keymap.set("n", "<A-l>", require("smart-splits").resize_right)
+			-- moving between splits
+			vim.keymap.set("n", "<C-h>", require("smart-splits").move_cursor_left)
+			vim.keymap.set("n", "<C-j>", require("smart-splits").move_cursor_down)
+			vim.keymap.set("n", "<C-k>", require("smart-splits").move_cursor_up)
+			vim.keymap.set("n", "<C-l>", require("smart-splits").move_cursor_right)
+			-- swapping buffers between windows
+			vim.keymap.set("n", "<leader><leader>h", require("smart-splits").swap_buf_left)
+			vim.keymap.set("n", "<leader><leader>j", require("smart-splits").swap_buf_down)
+			vim.keymap.set("n", "<leader><leader>k", require("smart-splits").swap_buf_up)
+			vim.keymap.set("n", "<leader><leader>l", require("smart-splits").swap_buf_right)
+		end,
+	},
 }
 
 vim.api.nvim_create_user_command("WhatHl", function()
--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -12,10 +12,10 @@ end
 
 local home = wezterm.home_dir
 
-local nu_paths = { home .. "/.cargo/bin/nu", "/usr/local/bin/nu" }
+local potential_nu_paths = { home .. "/.cargo/bin/nu", "/usr/local/bin/nu" }
 local nu_path = "nu"
 
-for _, file in ipairs(nu_paths) do
+for _, file in ipairs(potential_nu_paths) do
 	if file_exists(file) then
 		nu_path = file
 		break
@@ -47,5 +47,55 @@ config.enable_scroll_bar = true
 config.audible_bell = "Disabled"
 config.unicode_version = 15
 config.hide_mouse_cursor_when_typing = false
+
+local function is_vim(pane)
+	-- this is set by the smart-splits.nvim plugin, and unset on ExitPre in Neovim
+	return pane:get_user_vars().IS_NVIM == "true"
+end
+
+local direction_keys = {
+	Left = "h",
+	Down = "j",
+	Up = "k",
+	Right = "l",
+	-- reverse lookup
+	h = "Left",
+	j = "Down",
+	k = "Up",
+	l = "Right",
+}
+
+local function split_nav(resize_or_move, key)
+	return {
+		key = key,
+		mods = resize_or_move == "resize" and "ALT" or "CTRL",
+		action = wezterm.action_callback(function(win, pane)
+			if is_vim(pane) then
+				-- pass the keys through to vim/nvim
+				win:perform_action({
+					SendKey = { key = key, mods = resize_or_move == "resize" and "ALT" or "CTRL" },
+				}, pane)
+			else
+				if resize_or_move == "resize" then
+					win:perform_action({ AdjustPaneSize = { direction_keys[key], 3 } }, pane)
+				else
+					win:perform_action({ ActivatePaneDirection = direction_keys[key] }, pane)
+				end
+			end
+		end),
+	}
+end
+config.keys = {
+	-- 	-- move between split panes
+	split_nav("move", "h"),
+	split_nav("move", "j"),
+	split_nav("move", "k"),
+	split_nav("move", "l"),
+	-- resize panes
+	split_nav("resize", "h"),
+	split_nav("resize", "j"),
+	split_nav("resize", "k"),
+	split_nav("resize", "l"),
+}
 
 return config
