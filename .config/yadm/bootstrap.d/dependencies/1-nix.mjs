dc8325890a16a3b0f0ce6f82b212a102e0475f20
Joaquín Triñanes <hi@joaquint.io>
2023-07-13 19:21:20 +0200

    Install home-manager on bootstrap

--- .config/yadm/bootstrap.d/dependencies/1-nix.mjs
+++ .config/yadm/bootstrap.d/dependencies/1-nix.mjs
@@ -1,9 +1,17 @@
 #!/usr/bin/env -S zx --experimental
 
-if (fs.existsSync("/nix")) {
+import { commandExists } from "../utils/commands.mjs";
+
+if (!fs.existsSync("/nix")) {
+  await $`curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix`.pipe(
+    $`sh -s -- install`
+  );
+}
+
+if (commandExists("home-manager")) {
   process.exit();
 }
 
-await $`curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix`.pipe(
-  $`sh -s -- install`
-);
+await $`nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager`;
+await $`nix-channel --update`;
+await $`nix-shell '<home-manager>' -A install`;
