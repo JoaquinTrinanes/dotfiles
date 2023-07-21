4d304f92be2633a60de3e9368f19e4c7c2d74c8c
Joaquín Triñanes <hi@joaquint.io>
2023-07-21 19:16:19 +0200

    Set RUSTUP_HOME

--- .config/shell/posix/core/01-path@env.sh
+++ .config/shell/posix/core/01-path@env.sh
@@ -9,6 +9,7 @@ prepend_path () {
 
 export NVIM_HOME="$XDG_CONFIG_HOME/nvim"
 export RIPGREP_CONFIG_PATH="$XDG_CONFIG_HOME/ripgrep/config"
+export RUSTUP_HOME="$XDG_DATA_HOME/rustup"
 
 # Rust
 export CARGO_HOME="$XDG_DATA_HOME/cargo"
