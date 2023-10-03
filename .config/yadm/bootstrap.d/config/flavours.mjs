43982e0286a8389f40cb5848a8d9718a39e149d1
Joaquín Triñanes <hi@joaquint.io>
2023-10-03 17:47:42 +0200

    Fix bootstrap

--- .config/yadm/bootstrap.d/config/flavours.mjs
+++ .config/yadm/bootstrap.d/config/flavours.mjs
@@ -1,9 +1,19 @@
 #!/usr/bin/env -S zx --experimental
 
 import { createMacConfigSymlink } from "../utils/os.mjs";
+import { commandExists } from "../utils/commands.mjs";
 import * as log from "../utils/log.mjs";
 
-const DEFAULT_THEME = "nord";
+if (!commandExists("flavours")) {
+  log.warn("Flavours binary not found, skipping setup");
+  process.exit();
+}
+
+const DEFAULT_THEME = "catppuccin-frappe";
+
+process.env.PATH = `${path.join(os.homedir(), ".cargo/bin")}:${
+  process.env.PATH
+}`;
 
 createMacConfigSymlink("flavours");
 
@@ -13,7 +23,7 @@ try {
   await $`flavours current`.quiet();
 } catch (e) {
   log.info(
-    `No flavours theme detected. Applying default ${DEFAULT_THEME} theme`
+    `No flavours theme detected. Applying default ${DEFAULT_THEME} theme`,
   );
   await $`flavours apply ${DEFAULT_THEME}`;
 }
--- .config/yadm/bootstrap.d/config/gpg.mjs
+++ .config/yadm/bootstrap.d/config/gpg.mjs
@@ -1,4 +1,9 @@
 #!/usr/bin/env zx
 
-await $`cp /etc/xdg/autostart/gnome-keyring-ssh.desktop ~/.config/autostart/gnome-keyring-ssh.desktop`;
+const entryPath = "/etc/xdg/autostart/gnome-keyring-ssh.desktop";
+if (!fs.existsSync(entryPath)) {
+  process.exit();
+}
+
+await $`cp ${entryPath} ~/.config/autostart/gnome-keyring-ssh.desktop`;
 await $`echo 'Hidden=true' >> ~/.config/autostart/gnome-keyring-ssh.desktop`;
