c4ea91e74373d2cc374d23b647a047d508b4fb37
Joaquín Triñanes <hi@joaquint.io>
2023-11-04 11:05:36 +0100

    Show import paths in completer

--- .config/nvim/lua/plugins/completer.lua
+++ .config/nvim/lua/plugins/completer.lua
@@ -6,11 +6,36 @@ local M = {
   {
     "hrsh7th/nvim-cmp",
     dependencies = { "windwp/nvim-autopairs", event = "InsertEnter", opts = {} },
+    --- @param opts cmp.ConfigSchema
     opts = function(_, opts)
       local cmp_autopairs = require("nvim-autopairs.completion.cmp")
       local cmp = require("cmp")
       cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
-      opts.experimental.ghost_text.enabled = true
+      opts.experimental.ghost_text = true
+
+      local prev_format = opts.formatting.format
+      local max_menu_length = 15
+
+      opts.formatting.format = function(entry, _item)
+        local item = prev_format(entry, _item)
+        if entry.completion_item.detail ~= nil and entry.completion_item.detail ~= "" then
+          item.menu = entry.completion_item.detail
+          item.menu_hl_group = "Comment"
+
+          local truncated = string.sub(item.menu, 1, max_menu_length)
+          if #truncated < #item.menu then
+            item.menu = string.sub(truncated, 1, max_menu_length - 1) .. "…"
+          end
+          -- else
+          --   vim_item.menu = ({
+          --     nvim_lsp = "[LSP]",
+          --     luasnip = "[Snippet]",
+          --     buffer = "[Buffer]",
+          --     path = "[Path]",
+          --   })[entry.source.name]
+        end
+        return item
+      end
     end,
   },
 }
